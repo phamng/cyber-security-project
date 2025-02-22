c035fec974cf1461b73c4c700376a9e19171c0cd
Nga Pham <nga.pham@sok.fi>
2025-02-22 23:54:11 +0200

    A01:2021-Broken Access Control issue

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,32 +1,28 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-from django.views import generic
 
 from .models import Choice, Question
-from django.utils import timezone
+# from django.contrib.auth.decorators import login_required
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 
-class IndexView(generic.ListView):
-    template_name = 'polls/index.html'
-    context_object_name = 'latest_question_list'
-
-    def get_queryset(self):
-        return Question.objects.filter(
-            pub_date__lte=timezone.now()
-        ).order_by('-pub_date')[:5]
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
-class DetailView(generic.DetailView):
-    model = Question
-    template_name = 'polls/detail.html'
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
-
-class ResultsView(generic.DetailView):
-    model = Question
-    template_name = 'polls/results.html'
-
-
+# A01:2021 - Broken Access Control Fix: Ensure only authenticated users can vote
+# Fix: This ensures only logged-in users can access this view
+# @login_required
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
