1c1e9fb93dea0da436aaa9fb87c237d16c3198e4
Nga Pham <nga.pham@sok.fi>
2025-04-23 14:20:53 +0300

    include CSRF flaw

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -46,6 +46,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    # Cross-Site Request Forgery (CSRF) Flaw
     # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
